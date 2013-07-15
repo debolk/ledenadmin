c69f0d16eacfcc895d2adeb211b02d4f528964dd
Derk-Jan Karrenbeld <derk-jan+git@karrenbeld.info>
2013-07-15 16:16:02 +0200

    Fix placeholder images and required fields

--- app/js/views/person.js
+++ app/js/views/person.js
@@ -21,16 +21,16 @@
       return '<header>\
 			<div class="control-group">\
 				<div class="controls">\
-					<input class="input-big" id="name_first" name="input[blip][firstname]" placeholder="First name" maxlength="100" value="<%= firstname %>" required="" type="text">\
-					<input class="input-big" id="name_last" name="input[blip][lastname]" placeholder="Last name" maxlength="100" value="<%= lastname %>" required="" type="text">\
+					<input class="input-big" id="name_first" name="input[blip][firstname]" placeholder="First name" maxlength="100" value="<%= firstname %>" required type="text">\
+					<input class="input-big" id="name_last" name="input[blip][lastname]" placeholder="Last name" maxlength="100" value="<%= lastname %>" required type="text">\
 					<button data-action="toggleEdit"><i class="icon-pencil"></i></button>\
 				</div>\
 			</div>\
 			\
 			<div class="contact">\
-				<img src="//placehold.it/32x32.png&text=P"/> <input class="" id="contact_phone" name="input[blip][phone]" placeholder="" maxlength="20" value="<%= phone %>" required="" type="tel">\
-				<img src="//placehold.it/32x32.png&text=H"/> <input class="" id="contact_phone_home" name="input[blip][phone_parents]" placeholder="" maxlength="20" value="<%= phone_parents %>" required="" type="tel">\
-				<img src="//placehold.it/32x32.png&text=E"/> <input class="" id="contact_email" name="input[blip][email]" placeholder="" maxlength="20" value="<%= email %>" required="" type="tel">\
+				<img src="http://placehold.it/32x32.png&text=P"/> <input class="" id="contact_phone" name="input[blip][phone]" placeholder="" maxlength="20" value="<%= phone %>" type="tel">\
+				<img src="http://placehold.it/32x32.png&text=H"/> <input class="" id="contact_phone_home" name="input[blip][phone_parents]" placeholder="" maxlength="20" value="<%= phone_parents %>" type="tel">\
+				<img src="http://placehold.it/32x32.png&text=E"/> <input class="" id="contact_email" name="input[blip][email]" placeholder="" maxlength="20" value="<%= email %>" type="tel">\
 			</div>\
 		</header>\
 		\
